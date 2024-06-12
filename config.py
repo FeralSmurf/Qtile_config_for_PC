18d57be39723f5e2087e730635cf345ef0cc73d5
FeralSmurf <bogdanvarzaru@gmail.com>
2024-06-12 23:24:31 +0300

    latest config

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 from check_internet_widget import check_connectivity
+from weather_details import OpenWebsite
 
 mod = "mod4"
 terminal = guess_terminal()
@@ -14,9 +15,8 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
     Key(
         [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
     ),
@@ -28,19 +28,14 @@ keys = [
     ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
     Key(
         [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
     ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
     Key(
         [mod, "shift"],
         "Return",
@@ -48,8 +43,6 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key(
         [mod],
@@ -71,13 +64,16 @@ keys = [
     # language #
     ############
     Key([mod], "F1", lazy.spawn("setxkbmap us"), desc="Change to US layout"),
-    Key([mod], "F2", lazy.spawn("setxkbmap ro std"), desc="Change to RO-STD layout"),
+    Key([mod], "F2", lazy.spawn("setxkbmap ro std"),
+        desc="Change to RO-STD layout"),
 
     ##############
     # Media keys #
     ##############
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer sset Master 5%-"), desc="Lower Volume by 5%"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer sset Master 5%+"), desc="Raise Volume by 5%"),
+    Key([], "XF86AudioLowerVolume", lazy.spawn(
+        "amixer sset Master 5%-"), desc="Lower Volume by 5%"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn(
+        "amixer sset Master 5%+"), desc="Raise Volume by 5%"),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -86,28 +82,27 @@ groups = [
     Group("1", label="1 dev", spawn=["code"]),
     Group("2", label="2 edit", spawn=["alacritty"]),
     Group("3", label="3 www", spawn=["firefox"]),
-    Group("4", label="4 sys", spawn=["alacritty", "-e", "ranger"]),
+    Group("4", label="4 sys", spawn=["alacritty -e ranger", "thunar"]),
     Group("5", label="5 docs", ),
-    Group("6", label="6 media", spawn=["chromium youtube.com"] ),
+    Group("6", label="6 media", spawn=["chromium youtube.com"]),
     Group("7", label="7 misc", ),
 ]
 
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name),
             ),
         ]
     )
@@ -117,13 +112,13 @@ layouts = [
         border_focus="#a6e3a1",
         border_normal="#1e1e2e",
         border_width=2,
-        margin = 4,
+        margin=4,
         name="",
     ),
 ]
 
 widget_defaults = dict(
-    font="NotoNerdFont Bold", 
+    font="NotoNerdFont Bold",
     fontsize=14,
     padding=2,
 )
@@ -134,7 +129,8 @@ screens = [
         top=bar.Bar(
             [
                 widget.CurrentLayout(),
-                widget.GroupBox(this_current_screen_border="#a6e3a1", highlight_method="line", background="#1e1e2e", highlight_color="#1e1e2e"),
+                widget.GroupBox(this_current_screen_border="#a6e3a1", highlight_method="line",
+                                background="#1e1e2e", highlight_color="#1e1e2e"),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Chord(
@@ -145,20 +141,27 @@ screens = [
                 ),
                 widget.Systray(),
                 widget.Clipboard(fmt='Clipped {} '),
-                widget.GenPollText(func=check_connectivity, update_interval=10),
-                widget.OpenWeather(location='Bucharest', format='~{main_feels_like:.0f}°{units_temperature}, {weather_details}, {pressure}hPa, {wind_speed:.0f}km/h, {humidity}%H', fmt='🏙️ {}', app_key='0ec6327bcee56539cbf468aaffd0bb79'),
+                widget.GenPollText(func=check_connectivity,
+                                   update_interval=10),
+                widget.OpenWeather(
+                    location='Bucharest', format='~{main_feels_like:.0f}°{units_temperature}, {weather_details}, {pressure}hPa, {wind_speed:.0f}km/h, {humidity}%H', fmt='🏙️ {}', app_key='0ec6327bcee56539cbf468aaffd0bb79'),
+                OpenWebsite(),
                 widget.DF(
                     visible_on_warn=False,
                     fmt="💾 {} ",
                     format="{uf}{m}|{r:.1f}{m}|{r:.0f}%",
                     partition="/",
                 ),
-                widget.Memory(fmt = '🐏 {}', measure_mem='G', format='{MemUsed:.0f}{mm}|{MemTotal:.0f}{mm}'),
-                widget.CPU(fmt = '🧠 {}', format = '{freq_current}GHz|{load_percent:.0f}%', width = 110),
-                widget.ThermalSensor(fmt='🔥 {}', format='{temp:.0f}{unit}', tag_sensor="Package id 0"),
+                widget.Memory(fmt='🐏 {}', measure_mem='G',
+                              format='{MemUsed:.0f}{mm}|{MemTotal:.0f}{mm}'),
+                widget.CPU(
+                    fmt='🧠 {}', format='{freq_current}GHz|{load_percent:.0f}%', width=110),
+                widget.ThermalSensor(
+                    fmt='🔥 {}', format='{temp:.0f}{unit}', tag_sensor="Package id 0"),
                 widget.Volume(fmt='📢 {}'),
                 widget.Clock(format='%Y.%m.%d %a %I:%M', fmt='⏳️ {} '),
-                widget.KeyboardLayout(fmt='🎹 {}', configured_keyboards=["us", "ro"]),
+                widget.KeyboardLayout(
+                    fmt='🎹 {}', configured_keyboards=["us", "ro"]),
             ],
             24,
             background='#1e1e2e',
@@ -166,7 +169,6 @@ screens = [
     ),
 ]
 
-# Drag floating layouts.
 mouse = [
     Drag(
         [mod],
@@ -187,10 +189,9 @@ bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
-        border_focus="#a6e3a1",
-        border_normal="#1e1e2e",
+    border_focus="#a6e3a1",
+    border_normal="#1e1e2e",
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -204,11 +205,8 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
 wmname = "LG3D"
